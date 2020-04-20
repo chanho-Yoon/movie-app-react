236d403b31e28da4bcb7a30c689396e393d29c1c
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-20 23:27:09 +0900

    필요없는 부분 삭제하고 react test 진행

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div> GHello!!!오 쩐다 바로 변경된다 오 뭐냐 신기하네 </div>;
 }
 
 export default App;
