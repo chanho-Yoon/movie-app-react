4c2adf2cf6ca521efd72c8d512f12e7bba26bc9d
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-05-02 15:30:42 +0900

    Route about 삭제 및 Detail 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
-import About from './routes/About';
 import Detail from './routes/Detail';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
-      <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
