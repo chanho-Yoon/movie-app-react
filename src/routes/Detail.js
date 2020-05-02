61fe904fa7bfe3553aebcf583d983ddc284c6ea2
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-05-02 15:27:02 +0900

    클릭한 비디오 설명 전체보기 컴포넌트

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import MovieDetail from '../components/MovieDetail';
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello Detail</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push('/');
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <section className="detail">
+          <MovieDetail
+            year={location.state.year}
+            title={location.state.title}
+            summary={location.state.summary}
+            poster={location.state.poster}
+            genres={location.state.genres}
+          />
+        </section>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
